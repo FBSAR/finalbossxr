1695f662210b117dbedaa00efce78ea4aa5aa7d7
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2025-05-04 12:05:38 -0400

    Updated the 'About Us' Section on the Homepage / Green Active Links in Navbar (Desktop)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
   // Navbar Link Classes
   const nonActiveNavLink = 'text-[#fff]/60 text-xl lg:text-sm  my-1 duration-100 raleway';
-  const activeNavLink = 'text-[#fff] text-xl lg:text-sm lg:bg-transparent my-1 font-bold duration-100 raleway-700';
+  const activeNavLink = 'text-[#fff] text-xl lg:text-sm green-header-text lg:bg-transparent my-1 font-bold duration-100 raleway-700';
 
   // Should hide Navbar on mobile when a link is clicked
   let hideNavMenu = true;
