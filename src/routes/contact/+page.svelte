8f48cd8814d3e42e0a92be6c9ea51196737f6d25
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2025-10-10 16:38:36 -0400

    Deleted unused CSS Declarations

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,18 +11,4 @@
   <SocialMedia />
 </main>
 <style>
-  .contact-card, .contact-header-card {
-    transition: 200ms;
-  }
-  @media (min-width: 1000px) { 
-    .contact-header-card:hover {
-      font-size: 1.35em;
-    }
-    .contact-card:hover, .contact-header-card:hover{
-      transition: 200ms;
-      background: linear-gradient(135deg, #5f9f571a, #FFD7001a);
-      transform: translateY(-10px);
-      border-color: #0fd8414d;
-    }
-  }
 </style>
